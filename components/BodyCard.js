893ae74030303d79ce56c232da099b5e30afa147
ChrisAchinga <achinga.chris@gmail.com>
2021-04-09 01:20:33 +0300

    proptypes and default props

--- components/BodyCard.js
+++ components/BodyCard.js
@@ -1,26 +1,36 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 import { Card, Button } from 'react-bootstrap'
 import ModalBox from '@components/ModalBox'
 
-const BodyCard = () => {
+const BodyCard = ({ title, description, maxNum }) => {
   const [modalShow, setModalShow] = useState(false)
   return (
     <Card>
       <Card.Img variant='top' src='holder.js/100px180' />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Text>Maximum No. of People: {maxNum}</Card.Text>
         <Button variant='primary' onClick={() => setModalShow(true)}>
           Go somewhere
         </Button>
-
         <ModalBox show={modalShow} onHide={() => setModalShow(false)} />
       </Card.Body>
     </Card>
   )
+}
+
+BodyCard.defaultProps = {
+  title: 'Space Title',
+  description: 'A description of the space',
+  maxNum: 20,
+}
+
+BodyCard.propTypes = {
+  maxNum: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
 }
 
 export default BodyCard
