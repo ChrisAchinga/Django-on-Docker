e40eedd353ee8c2c1ff8e8547cc1c6429b7906af
ChrisAchinga <achinga.chris@gmail.com>
2021-04-09 01:05:11 +0300

    import components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,27 @@
 import Head from 'next/head'
+import { Container } from 'react-bootstrap'
 import Header from '@components/Header'
+import Menu from '@components/Menu'
+import Body from '@components/Body'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className='container'>
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Community Space</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      <Menu />
       <main>
-        <Header title='Welcome to my app!' />
-        <p className='description'>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Container>
+          <Header
+            title='Community Space'
+            description='Book or Reserve a Space'
+          />
+          <Body />
+        </Container>
       </main>
 
       <Footer />
