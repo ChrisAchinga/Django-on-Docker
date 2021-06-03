2b60ba913cbd20221998485ab9e7d6bc66338126
ChrisAchinga <achinga.chris@gmail.com>
2021-06-03 18:28:00 +0300

    //

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className='pt-5 my-5 text-muted border-top'>
-      SwahiliPot Hub Mock Website | ChrisDevCode &copy; 2021
+    <footer className='pt-5 my-5 text-muted border-top text-center justify-content-center'>
+      Swahilipot Hub
     </footer>
   )
 }
--- components/homePage/MemberBenefit.js
+++ components/homePage/MemberBenefit.js
@@ -1,49 +1,34 @@
 import { FaRegStar } from 'react-icons/fa'
 import { Col, Row, Container, Card } from 'react-bootstrap'
 
+const InnerCard = () => {
+  return (
+    <Card className='h-100'>
+      <Card.Body>
+        <Card.Title>
+          <FaRegStar />
+        </Card.Title>
+        <Card.Text>Text</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
 const MemberBenefit = () => {
   return (
     <Container>
       <Row>
         <Col lg={3} md={4} sm={6} className='mb-4'>
-          <Card className='h-100'>
-            <Card.Body>
-              <Card.Title>
-                <FaRegStar />
-              </Card.Title>
-              <Card.Text>Text</Card.Text>
-            </Card.Body>
-          </Card>
+          <InnerCard />
         </Col>
         <Col lg={3} md={4} sm={6} className='mb-4'>
-          <Card className='h-100'>
-            <Card.Body>
-              <Card.Title>
-                <FaRegStar />
-              </Card.Title>
-              <Card.Text>Text</Card.Text>
-            </Card.Body>
-          </Card>
+          <InnerCard />
         </Col>
         <Col lg={3} md={4} sm={6} className='mb-4'>
-          <Card className='h-100'>
-            <Card.Body>
-              <Card.Title>
-                <FaRegStar />
-              </Card.Title>
-              <Card.Text>Text</Card.Text>
-            </Card.Body>
-          </Card>
+          <InnerCard />
         </Col>
         <Col lg={3} md={4} sm={6} className='mb-4'>
-          <Card className='h-100'>
-            <Card.Body>
-              <Card.Title>
-                <FaRegStar />
-              </Card.Title>
-              <Card.Text>Text</Card.Text>
-            </Card.Body>
-          </Card>
+          <InnerCard />
         </Col>
       </Row>
     </Container>
