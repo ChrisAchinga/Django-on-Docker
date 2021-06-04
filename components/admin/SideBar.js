9a031e746b1afab1c5cf6003403062dc95af1802
ChrisAchinga <achinga.chris@gmail.com>
2021-06-04 15:32:55 +0300

    side bar links

--- components/admin/SideBar.js
+++ components/admin/SideBar.js
@@ -1,58 +1,78 @@
+import Link from 'next/link'
+
 const SideBar = () => {
   return (
     <nav id='sidebarMenu' className='d-md-block bg-light sidebar collapse'>
-       <div className="position-sticky pt-3">
-        <ul className="nav flex-column">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Dashboard
-            </a>
+      <div className='position-sticky pt-3'>
+        <ul className='nav flex-column'>
+          <li className='nav-item'>
+            <Link href='/admin'>
+              <a className='nav-link active' aria-current='page'>
+                Dashboard
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Members
-            </a>
+          <li className='nav-item'>
+            <Link href='/members'>
+              <a className='nav-link' >
+                Members
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Board Members
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/members/board'>
+              <a className='nav-link' >
+                Board Members
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Volunteers
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/volunteers'>
+              <a className='nav-link' >
+                Volunteers
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Admin Users
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/users'>
+              <a className='nav-link' >
+                Admin Users
+              </a>
+            </Link>
           </li>
         </ul>
 
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <h6 className='sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted'>
           <span>Activities & Updates</span>
         </h6>
-        <ul className="nav flex-column mb-2">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Updates and News
-            </a>
+        <ul className='nav flex-column mb-2'>
+          <li className='nav-item'>
+            <Link href='/admin/updates'>
+              <a className='nav-link' >
+                Updates and News
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Events
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/events'>
+              <a className='nav-link' >
+                Events
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Blogs
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/blogs'>
+              <a className='nav-link' >
+                Blogs
+              </a>
+            </Link>
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Space
-            </a>
+          <li className='nav-item'>
+            <Link href='/admin/space'>
+              <a className='nav-link' >
+                Space
+              </a>
+            </Link>
           </li>
         </ul>
       </div>
