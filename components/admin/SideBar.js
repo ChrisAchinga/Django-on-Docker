12741d1c90365dfc3be2f6d5e31d61aa6f5ed92d
ChrisAchinga <achinga.chris@gmail.com>
2021-06-04 15:18:25 +0300

    menu and sidebar

--- components/admin/SideBar.js
+++ components/admin/SideBar.js
@@ -1,9 +1,63 @@
 const SideBar = () => {
-    return (
-        <div>
-            Sidebar
-        </div>
-    )
+  return (
+    <nav id='sidebarMenu' className='d-md-block bg-light sidebar collapse'>
+       <div className="position-sticky pt-3">
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">
+              Dashboard
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Members
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Board Members
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Volunteers
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Admin Users
+            </a>
+          </li>
+        </ul>
+
+        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Activities & Updates</span>
+        </h6>
+        <ul className="nav flex-column mb-2">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Updates and News
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Events
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Blogs
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Space
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default SideBar
--- components/admin/TopMenu.js
+++ components/admin/TopMenu.js
@@ -1,9 +1,29 @@
 const TopMenu = () => {
-    return (
-        <div>
-            top menu
-        </div>
-    )
+  return (
+    <header className='navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow'>
+      <a className='navbar-brand col-md-3 col-lg-2 me-0 px-3' href='#'>
+        SwahiliPot Hub Admin
+      </a>
+      <button
+        className='navbar-toggler position-absolute d-md-none collapsed'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#sidebarMenu'
+        aria-controls='sidebarMenu'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <ul className='navbar-nav px-3'>
+        <li className='nav-item text-nowrap'>
+          <a className='nav-link' href='#'>
+            Sign out
+          </a>
+        </li>
+      </ul>
+    </header>
+  )
 }
 
 export default TopMenu
